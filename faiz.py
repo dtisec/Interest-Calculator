d55844ce6687ae6f7e0b198286bd7e6bb79cfeb2
Doğukan <51451598+LewisVR@users.noreply.github.com>
2022-12-26 23:37:51 +0300

    Create faiz.py

---
+++ faiz.py
@@ -0,0 +1,7 @@
+faiz_orani = float(input("Faiz oranı girin :"))
+vade = int(input("Faiz vadesi(yıl) :"))
+anapara = float(input("Anaparayı girin :"))
+ 
+faiz = faiz_orani * vade * anapara
+ 
+print("Hesaplanan faiz",faiz)
